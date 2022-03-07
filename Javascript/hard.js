72c99d26484ce7369766ab85da2b7658d7c774c1
Bayramzade066 <bayramzade.313@gmail.com>
2022-03-08 01:11:31 +0400

    ads

--- Javascript/hard.js
+++ Javascript/hard.js
@@ -94,7 +94,6 @@ const getHoverDirection = function (event) {
   
           var dir = getHoverDirection(event);
   
-          // Reset classes.
           // event.currentTarget.className = 'item hover';
           // > If support for IE11 is not needed.
           // event.currentTarget.classList.remove('mouseenter', 'mouseleave', 'top', 'right', 'bottom', 'left');
